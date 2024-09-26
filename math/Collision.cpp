56bb848966ed7852a1ddead2e83d847917c9b9ce
Jon Tor <zirehl12@gmail.com>
2024-09-26 23:39:18 +0700

    add method to check intersect between line segments

--- math/Collision.cpp
+++ math/Collision.cpp
@@ -3,3 +3,15 @@
 void Collision::circle_vs_rectangle(Circle& circle, Rectangle& rectangle, const Line& circle_relative_velocity)
 {
 }
+
+bool Collision::intersect(const Line &line1, const Line &line2, float &t)
+{
+    Vector A = line1.get_end() - line1.get_start();
+    Vector B = line2.get_end() - line2.get_start();
+    Vector C = line1.get_start() - line2.get_start();
+
+    float numerator = B.get_y() * C.get_x() - B.get_x() * C.get_y();
+    float denominator = A.get_y() * B.get_x() - A.get_x() * B.get_y();
+
+    return false;
+}
--- math/Collision.h
+++ math/Collision.h
@@ -9,4 +9,6 @@ public:
     static void circle_vs_rectangle(Circle& circle, Rectangle& rectangle, const Line& circle_relative_velocity);
     static void circle_inside_rectangle(Circle& inner_circle, Rectangle& outer_rectangle, const Line& circle_relative_velocity);
 private:
+    static bool intersect(const Line& line1, const Line& line2, float& t);
+    static bool intersect(const Line& line, const Circle& circle, float& t);
 };
